6bf3d5ad431b847c173213da5f20ca65f4625be8
Thamires Lopes <thamiresl686@gmail.com>
2020-04-18 15:44:18 -0300

    adding a temporary view (welcome view)

---
+++ lib/views/welcomeView.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//this is a temporary view
+
+class WelcomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Welcome"),
+    );
+  }
+}
