8cdff10cc169db5d2bf066bf804935ebd6466dee
Thamires Lopes <thamiresl686@gmail.com>
2020-04-18 14:48:41 -0300

    basic login view

--- lib/views/loginView.dart
+++ lib/views/loginView.dart
@@ -4,7 +4,39 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      //backgroundColor: Colors.blueAccent,
+      body: Center(
+        child: Container(
+          height: 300,
+          width: 300,
+          child: Column(children: <Widget>[
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: "E-mail",
+                labelStyle: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: "Password",
+                labelStyle: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ]),
+        ),
+      ),
     );
   }
 }
